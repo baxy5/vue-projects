418e169cc7ae1e6ff08f1374fc45e9fc3afe20a6
baxy5 <bakos.soda@gmail.com>
2022-04-14 22:15:27 +0200

    keyframes and fade animation

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,84 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Infinty from "./components/Infinty.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Infinty },
+  data() {
+    return {
+      fadeInElements: "",
+    };
+  },
+  methods: {
+    fadeIn() {
+      window.addEventListener("scroll", () => {
+        for (let i = 0; i < this.fadeInElements.length; i++) {
+          let elem = this.fadeInElements[i];
+          let distInView =
+            elem.getBoundingClientRect().top - window.innerHeight + 400;
+
+          if (distInView < 0) {
+            elem.classList.add("fadeIn");
+          } else {
+            elem.classList.remove("fadeIn");
+            document.getElementsByClassName("container").style.opacity = 0;
+          }
+        }
+      });
+    },
+  },
+  mounted() {
+    this.fadeInElements = Array.from(
+      document.getElementsByClassName("container")
+    );
+    this.fadeIn();
+  },
+};
 </script>
+
+<template>
+  <Infinty />
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+  <div class="container">
+    <div class="text-container">
+      <h1>FADE SCROLL</h1>
+    </div>
+  </div>
+</template>
 
 <style>
 #app {
@@ -21,6 +87,35 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+}
+.container {
+  height: 50vh;
+  border: 2px solid black;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 10px;
+  webkit-transform: translateY(50px) rotate(-5deg) translateZ(0);
+  transform: translateY(50px) rotate(-5deg) translateZ(0);
+  -webkit-transition-delay: 0.3s;
+  -o-transition-delay: 0.3s;
+  transition-delay: 0.3s;
+  -webkit-transition: 0.4s;
+  -o-transition: 0.4s;
+  transition: 0.4s;
+  opacity: 0;
+  -webkit-filter: grayscale(1);
+  filter: grayscale(1);
+}
+.fadeIn {
+  opacity: 1;
+  -webkit-transform: translateY(0px) rotate(0deg) translateZ(0);
+  transform: translateY(0px) rotate(0deg) translateZ(0);
+}
+.text-container {
+  background-color: tomato;
+  padding: 10px;
 }
 </style>
---
+++ src/components/Infinty.vue
@@ -0,0 +1,32 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <h1 class="infinity">Adaptics</h1>
+  <img
+    src="@/assets/navigation.png"
+    height="100"
+    width="100"
+    class="infinity"
+  />
+  <img src="@/assets/world.png" height="100" width="100" class="infinity" />
+</template>
+
+<style>
+.infinity {
+  animation: infinity 5s linear infinite;
+  text-shadow: 2px 2px;
+}
+@keyframes infinity {
+  0% {
+    transform: rotate3d(0, 1, 0, 0deg);
+  }
+  50% {
+    transform: rotate3d(0, 1, 0, 180deg);
+  }
+  100% {
+    transform: rotate3d(0, 1, 0, 0deg);
+  }
+}
+</style>
